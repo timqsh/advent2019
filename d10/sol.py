caa18ada1e256e2d53ff647f92e7868284533335
timqsh <timqsh@gmail.com>
2019-12-17 02:35:23 +0500

    d10_2

--- d10/sol.py
+++ d10/sol.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Tuple, List, Optional
+from typing import NamedTuple, Tuple, List, Callable
 from math import gcd
 
 
@@ -20,13 +20,16 @@ class Pos(NamedTuple):
 
 
 class Grid(list):
-    def __init__(self, li: List[str]):
+    def __init__(self, li: List[List[str]]):
         self.width = len(li[0])
         self.height = len(li)
         return super().__init__(li)
 
     def pos(self, pos: Pos) -> str:
         return self[pos.y][pos.x]
+
+    def set_pos(self, pos: Pos, value: str) -> None:
+        self[pos.y][pos.x] = value
 
     def asteroids(self) -> List[Pos]:
         res = []
@@ -50,22 +53,39 @@ class Grid(list):
                 return False
         return True
 
-    def visible_num(self, pos: Pos) -> int:
-        count = 0
+    def visible_from(self, pos: Pos) -> List[Pos]:
+        res = []
         for a in self.asteroids():
             if a != pos and self.is_visible(pos, a):
-                count += 1
-        return count
+                res.append(a)
+        return res
 
-    def best_pos(self) -> Tuple[Optional[Pos], int]:
+    def best_pos(self) -> Tuple[Pos, int]:
         cur_max = 0
-        cur_pos = None
+        cur_pos: Pos
         for a in self.asteroids():
-            n = self.visible_num(a)
+            n = len(self.visible_from(a))
             if n > cur_max:
                 cur_max = n
                 cur_pos = a
         return cur_pos, cur_max
+
+
+def clockwise_around(center: Pos) -> Callable:
+    def clockwise_order(p: Pos) -> Tuple[int, float]:
+        dy, dx = (p.y - center.y), (p.x - center.x)
+        if dx == 0 and dy < 0:  # 12 o'clock
+            return 1, 0
+        elif dx > 0:
+            return 2, dy / dx
+        elif dx == 0 and dy > 0:  # 6 o'clock
+            return 3, 0
+        elif dx < 0:
+            return 4, dy / dx
+        else:
+            raise ValueError("sorting error")
+
+    return clockwise_order
 
 
 if __name__ == "__main__":
@@ -73,7 +93,19 @@ if __name__ == "__main__":
 
     text = sys.stdin.read()
 
-    grid = Grid(text.splitlines())
+    grid = Grid([[*s] for s in text.splitlines()])
+    best_pos, best_num = grid.best_pos()
+    print(f"best: {best_pos} ({best_num})")
 
-    res = grid.best_pos()
-    print(res)
+    i = 0
+    while True:
+        asteroids = grid.visible_from(best_pos)
+        if not asteroids:
+            break
+
+        asteroids.sort(key=clockwise_around(best_pos))
+        for asteroid in asteroids:
+            i += 1
+            grid.set_pos(asteroid, ".")
+            if i == 200:
+                print(f"{i}: {asteroid}")
