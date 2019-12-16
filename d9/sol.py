17ff4d28674e36e37e53e4646a6b7e187d1890aa
timqsh <timqsh@gmail.com>
2019-12-17 00:54:51 +0500

    refactor

--- d9/sol.py
+++ d9/sol.py
@@ -1,19 +1,27 @@
+from typing import List
+
+
 class Computer:
-    def __init__(self, memory, input):
+    POSITION_MODE = 0
+    IMMEDIATE_MODE = 1
+    RELATIVE_MODE = 2
+    JUMPED = 1
+
+    def __init__(self, memory: List[int], input: List[int] = None):
         self.memory = memory + [0] * 100_000
-        self.input = input
-        self.output = []
+        self.input = input or []
+        self.output: List[int] = []
         self.func_and_arg_num = {
-            1: (self.f_add, 3),
-            2: (self.f_mul, 3),
-            3: (self.f_input, 1),
-            4: (self.f_output, 1),
-            5: (self.f_jump_true, 2),
-            6: (self.f_jump_false, 2),
-            7: (self.f_less, 3),
-            8: (self.f_equals, 3),
-            9: (self.f_shift_base, 1),
-            99: (self.f_stop, 0),
+            1: (self.add, 3),
+            2: (self.mul, 3),
+            3: (self.read, 1),
+            4: (self.write, 1),
+            5: (self.jump_true, 2),
+            6: (self.jump_false, 2),
+            7: (self.less, 3),
+            8: (self.equals, 3),
+            9: (self.shift_base, 1),
+            99: (self.stop, 0),
         }
         self.pointer = 0
         self.base = 0
@@ -24,31 +32,19 @@ class Computer:
     def run(self):
         self.waiting = False
         while not self.stopped and not self.waiting:
-            self.iteration += 1
             op_code_and_params = self.memory[self.pointer]
-            self.pointer += 1
-
             params_code, op_code = divmod(op_code_and_params, 100)
             func, arg_num = self.func_and_arg_num[op_code]
             params = self.decode_params(params_code, arg_num)
+            args = self.read_args(params, arg_num)
 
-            args = []
-            for i in range(arg_num):
-                if params[i] == 0:  # position mode
-                    a = self.memory[self.pointer + i]
-                elif params[i] == 1:  # immediate mode
-                    a = self.pointer + i
-                elif params[i] == 2:  # relative mode
-                    a = self.memory[self.pointer + i] + self.base
-                else:
-                    raise ValueError(f"unexpected param mode {params[i]}")
-                args.append(a)
-
-            func(*args)
-            self.pointer += arg_num
+            jumped = func(*args)
+            if not jumped:
+                self.pointer += arg_num + 1
+            self.iteration += 1
 
     @staticmethod
-    def decode_params(params_code, arg_num):
+    def decode_params(params_code: int, arg_num: int) -> List[int]:
         result = []
         rest = params_code
         for i in range(arg_num):
@@ -56,40 +52,57 @@ class Computer:
             result.append(value)
         return result
 
-    def f_input(self, val):
+    def read_args(self, params: List[int], arg_num: int) -> List[int]:
+        args = []
+        for i in range(arg_num):
+            pos = self.pointer + i + 1
+            if params[i] == self.POSITION_MODE:
+                arg = self.memory[pos]
+            elif params[i] == self.IMMEDIATE_MODE:
+                arg = pos
+            elif params[i] == self.RELATIVE_MODE:
+                arg = self.memory[pos] + self.base
+            else:
+                raise ValueError(f"unexpected param mode {params[i]}")
+            args.append(arg)
+        return args
+
+    def add(self, a, b, res):
+        self.memory[res] = self.memory[a] + self.memory[b]
+
+    def mul(self, a, b, res):
+        self.memory[res] = self.memory[a] * self.memory[b]
+
+    def read(self, val):
         if self.input:
             self.memory[val] = self.input.pop(0)
         else:
             self.waiting = True
-            self.pointer -= 2
+            return self.JUMPED
 
-    def f_output(self, v):
+    def write(self, v):
         self.output.append(self.memory[v])
 
-    def f_add(self, a, b, res):
-        self.memory[res] = self.memory[a] + self.memory[b]
-
-    def f_mul(self, a, b, res):
-        self.memory[res] = self.memory[a] * self.memory[b]
-
-    def f_jump_true(self, condition, address):
+    def jump_true(self, condition, address):
         if self.memory[condition]:
-            self.pointer = self.memory[address] - 2
+            self.pointer = self.memory[address]
+            return self.JUMPED
 
-    def f_jump_false(self, condition, address):
+    def jump_false(self, condition, address):
         if not self.memory[condition]:
-            self.pointer = self.memory[address] - 2
+            self.pointer = self.memory[address]
+            return self.JUMPED
 
-    def f_less(self, a, b, res):
+    def less(self, a, b, res):
         self.memory[res] = int(self.memory[a] < self.memory[b])
 
-    def f_equals(self, a, b, res):
+    def equals(self, a, b, res):
         self.memory[res] = int(self.memory[a] == self.memory[b])
 
-    def f_shift_base(self, v):
+    def shift_base(self, v):
         self.base += self.memory[v]
 
-    def f_stop(self):
+    def stop(self):
         self.stopped = True
 
 
@@ -99,7 +112,7 @@ if __name__ == "__main__":
     text = sys.stdin.read()
     memory = [*map(int, text.split(","))]
 
-    computer = Computer(memory, [])
+    computer = Computer(memory)
     if len(sys.argv) > 1:
         computer.input.append(int(sys.argv[1]))
     computer.run()
